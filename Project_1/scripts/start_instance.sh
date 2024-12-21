986d82f9e83f27a68b9cb61307a5aa06aa1642d4
gabrielng-rj99 <gabrielng.rj99@gmail.com>
2024-12-21 06:24:50 -0300

    fix: crontab edit and crontab sudo
    out: the command
    out: crontab -> sudo ...
    in:  execute .sh script

--- Project_1/scripts/start_instance.sh
+++ Project_1/scripts/start_instance.sh
@@ -7,8 +7,8 @@ sudo cp -R ~/PB-CompassUOL-GabrielGomesDev/website/* /usr/share/nginx/html
 
 sudo systemctl restart crond && sudo systemctl restart nginx
 
-VERIFY_NGINX="*/5 * * * * sudo ~/PB-CompassUOL-GabrielGomesDev/Project_1/scripts/verify-nginx.sh"
-START_INSTANCE="@reboot sudo ~/PB-CompassUOL-GabrielGomesDev/Project_1/scripts/start_instance.sh"
+VERIFY_NGINX="*/5 * * * * ~/PB-CompassUOL-GabrielGomesDev/Project_1/scripts/verify-nginx.sh"
+START_INSTANCE="@reboot ~/PB-CompassUOL-GabrielGomesDev/Project_1/scripts/start_instance.sh"
 
 (sudo crontab -l | sudo grep -q "$VERIFY_NGINX") || (sudo crontab -l; sudo echo "$VERIFY_NGINX") | sudo crontab -
 (sudo crontab -l | sudo grep -q "$START_INSTANCE") || (sudo crontab -l; sudo echo "$START_INSTANCE") | sudo crontab -
