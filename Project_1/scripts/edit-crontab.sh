ba30181d6fcf9a3142e4506da7b1c278ae856d24
gabrielng-rj99 <gabrielng.rj99@gmail.com>
2024-12-21 06:02:56 -0300

    feat/fix: start_intance and ec2_user-data

--- Project_1/scripts/edit-crontab.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Comando para adicionar
-NOVO_COMANDO="0 5 * * * /caminho/para/seu/script.sh"
-
-# Verifica se o comando já existe no crontab
-(crontab -l | grep -q "$NOVO_COMANDO") || (crontab -l; echo "$NOVO_COMANDO") | crontab -
