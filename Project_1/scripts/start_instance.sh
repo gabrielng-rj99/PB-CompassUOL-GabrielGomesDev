615b021e95a39bb6b300cba49d901f6a9c436bce
gabrielng-rj99 <gabrielng.rj99@gmail.com>
2024-12-21 07:19:12 -0300

    fix: sudo or not sudo
    start_instance vs verify-nginx

--- Project_1/scripts/start_instance.sh
+++ Project_1/scripts/start_instance.sh
@@ -5,9 +5,13 @@ sudo rm -rf ~/PB-CompassUOL-GabrielGomesDev/
 git clone https://github.com/gabrielng-rj99/PB-CompassUOL-GabrielGomesDev
 sudo cp -R ~/PB-CompassUOL-GabrielGomesDev/website/* /usr/share/nginx/html
 
-sudo systemctl restart crond && sudo systemctl restart nginx
+sudo systemctl restart crond
+
+sudo systemctl stop nginx
+sudo bash ~/PB-CompassUOL-GabrielGomesDev/Project_1/scripts/verify-nginx.sh
+sudo systemctl start nginx
 
 VERIFY_NGINX="*/5 * * * * sudo bash ~/PB-CompassUOL-GabrielGomesDev/Project_1/scripts/verify-nginx.sh"
-START_INSTANCE="@reboot sudo bash ~/PB-CompassUOL-GabrielGomesDev/Project_1/scripts/start_instance.sh"
+START_INSTANCE="@reboot bash ~/PB-CompassUOL-GabrielGomesDev/Project_1/scripts/start_instance.sh"
 
 (crontab -l 2>/dev/null; echo "$VERIFY_NGINX"; echo "$START_INSTANCE") | crontab -
