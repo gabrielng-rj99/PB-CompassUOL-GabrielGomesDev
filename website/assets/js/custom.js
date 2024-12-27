bc385f912497ab9c3be10ff2d0b9175b7fc65d90
gabrielng-rj99 <gabrielng.rj99@gmail.com>
2024-12-27 05:52:00 -0300

    feature/fix: performance
    - compressed images
    - .gif -> .mp4
    - loading changes
    - script (waiting) changes

--- website/assets/js/custom.js
+++ website/assets/js/custom.js
@@ -74,12 +74,12 @@ $(document).ready(function(){
 
     // 4. welcome animation support
 
-        $(window).on('load', function(){
+        $(window).on('DOMContentLoaded', function(){
         	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
             $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
         });
 
-        $(window).on('load', function(){
+        $(window).on('DOMContentLoaded', function(){
         	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
             $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
         });
